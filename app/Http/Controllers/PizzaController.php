b89551b82a7c8c78e34aa7034ece4e7cebed9a9b
vikingiwan <vikingiwan@gmail.com>
2021-12-31 18:49:21 -0500

    handed funcs over to new PizzaController

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class PizzaController extends Controller
 {
-    //
+    public function index(){
+        $pizzas = [
+            ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+            ['type' => 'volcano', 'base' => 'garlic crust'],
+            ['type' => 'veg supreme', 'base' => 'thin & crispy']
+        ];
+    
+        return view('pizzas', [
+            'pizzas' => $pizzas
+        ]);
+    }
+
+    public function show($id){
+        return view('details', ['id' => $id]);
+    }
 }
