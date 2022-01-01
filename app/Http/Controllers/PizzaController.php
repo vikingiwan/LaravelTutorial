3aea952e3747b6262787a1154ae6edb71d8e7f70
vikingiwan <vikingiwan@gmail.com>
2021-12-31 21:47:10 -0500

    ep15 end

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -9,7 +9,10 @@ class PizzaController extends Controller
 {
     public function index(){
         
-        $pizzas = Pizza::all();
+        //$pizzas = Pizza::all();
+        //$pizzas = Pizza::orderBy('name', 'desc')->get();
+        //$pizzas = Pizza::where('type', 'hawaiian')->get();
+        $pizzas = Pizza::latest()->get();
     
         return view('pizzas', [
             'pizzas' => $pizzas
