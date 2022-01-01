5d5a1bedceea3c247b272b66747d4a361e7922af
vikingiwan <vikingiwan@gmail.com>
2022-01-01 05:34:23 -0500

    updated form action & method - end of ep 18

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="wrapper create-pizza">
         <h1>Create a New Pizza</h1>
-        <form action="" method="">
+        <form action="/pizzas" method="POST">
             <label for="name">Your name:</label>
             <input type="text" id="name" name="name">
             <label for="type">Choose Pizza Type:</label>
